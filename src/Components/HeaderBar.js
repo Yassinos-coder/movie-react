03f2481725eb40a2b9c591d483b545b7667e31b9
Yassinos-Coder <castroyassine0@gmail.com>
2022-10-03 17:19:08 +0100

    All good

--- src/Components/HeaderBar.js
+++ src/Components/HeaderBar.js
@@ -1,14 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/free-regular-svg-icons";
-import "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 const HeaderBar = () => {
   return (
     <>
       <div className="Header">
         <div className="Header-title-left">
-          
+          <p className="p-header-left">Anime-Site</p>
+          <p className="p2-header-left">Home</p>
+        </div>
+        <div className="search-bar-header">
+          <div className="search-bar-input">
+            <input className="Search-bar" type="text" placeholder="Filter Movie By Title" />
+            <button className="Search-bar-button">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </div>
+        <div className="signin-button">
+          <button className="Button-signin">
+            <p>Signin <FontAwesomeIcon icon={faRightToBracket}/></p>
+          </button>
         </div>
       </div>
     </>
